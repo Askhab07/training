7661369b902231996d12b586b792b844631185bd
Askhab <akhtaevaskhab07@mail.ru>
2024-11-14 18:53:35 +0300

    new update

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'Training',
+        short_name: 'App',
+        description: 'A simple app',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone', // Здесь добавляем параметр
+        start_url: '/',
+        icons: [
+          {
+            src: 'icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   base: '/training/'
 })
